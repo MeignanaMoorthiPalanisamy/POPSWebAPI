5257c5ea4aea5167bfe08fa13000365b7f1328ff
MeignanaMoorthiPalanisamy <MeignanaMoorthi.P@cognizant.com>
2018-12-14 16:34:37 +0530

    Add project files.

---
+++ POPSWebAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace POPSWebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ POPSWebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="POPSWebAPI.WebApiApplication" Language="C#" %>
